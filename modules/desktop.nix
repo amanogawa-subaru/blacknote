f2543c85be64145cffb4511fda7faa800511e12c
Amanogawa Subaru <amanogawa.subaru0705@gmail.com>
2026-09-22 07:38:57 +0800

    initial blacknote build

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
   # Sway as the compositor
-  programs.sway.enable = true;
+  programs.sway = {
+    enable = true;
+    
+    # Conditionally apply hide unsupported driver flag on nvidia systems
+    extraOptions = 
+      lib.optionals
+        (lib.elem "nvidia" config.services.xserver.videoDrivers)
+	[ "--unsupported-gpu" ];
+  };  
 
   # UWSM as the standard desktop "manager"
   programs.uwsm.enable = true;
